773b85f3d753514c9606c26a9882dac2144d5886
JhenJhenYe <dindincn111@gmail.com>
2021-12-05 21:27:50 +0800

    Add: Customize.vue, CustomizeSetter.vue

---
+++ src/components/customize/CustomizeSetter.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="raka-customize">
+    <ul class="raka-customize__list">
+      <li 
+        v-for="(element, index) of watchElements"
+        :key="index"
+        class="raka-customize__item" 
+        @click="clickTab(element)">{{ element }}</li>
+    </ul>
+    <div class="raka-customize__selector">
+      <div v-show="selectedWatchElement === 'FRAME'" style="display: flex">
+        <a
+          v-for="(frame, index) of frameList"
+          :key="index"
+          @click="setWatchElement('frame', frame.title)"
+        >
+          <img :src="frame.imgUrl" style="width: 100px" />
+          <div>{{ frame.title }}</div>
+        </a>
+      </div>
+      <div v-show="selectedWatchElement === 'OUTWARD'">
+        <button type="button" @click="setWatchElement('outward', '11111')">
+          1111
+        </button>
+      </div>
+      <div v-show="selectedWatchElement === 'STRAPE'">STRAPE</div>
+      <div v-show="selectedWatchElement === 'HANDS'">HANDS</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomizeSetter",
+  data() {
+    return {
+        selectedWatchElement: 'FRAME',
+        watchElements: ['FRAME', 'OUTWARD', 'STRAPE', 'HANDS'],
+        myCustomWatch: {
+          frame: '',
+          outward: '',
+          strape: '',
+          hands: '',
+        },
+      frameList: [
+        {
+          title: '玫瑰金',
+          imgUrl:
+            'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MKY23ref_VW_34FR+watch-44-alum-gold-nc-se_VW_34FR_WF_CO_GEO_TW?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1630364847000%2C1630708211000',
+        },
+        {
+          title: '太空灰',
+          imgUrl:
+            'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MKUQ3_VW_34FR+watch-45-alum-midnight-nc-7s_VW_34FR_WF_CO?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1632171067000,1631661671000',
+        },
+      ],
+    }
+  },
+  methods: {
+    setWatchElement(elementName, elementValue) {
+      this.myCustomWatch[elementName] = elementValue;
+    },
+    clickTab(elementName) {
+      this.selectedWatchElement = elementName;
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Customize.vue
+++ src/views/Customize.vue
@@ -1,15 +1,86 @@
 <template>
   <div>
-      TheCustomizePage works
+ <div class="raka-customize__page">
+      <div class="raka-customize-header">
+        <input type="text" v-model="myWatchTitle" />
+      </div>
+      <div class="raka-customize-body" style="display: flex; flex-direction: row">
+        <div class="raka-customize-body__aside">
+          <nav>
+            <ul class="raka-customize__manu-list">
+              <li class="raka-customize__manu-item" 
+                v-for="(feature, index) of featureList"
+                :key="index"
+                @click="selectedFeature = feature"
+                :class="{ 'active': selectedFeature === feature}">
+                {{ feature }}</li>
+            </ul>
+          </nav>
+        </div>
+        <div class="raka-customize-body__main">
+          <template v-if="selectedFeature === 'SURFACE'">
+            <CustomizeSetter
+                ref="customizeSetter" />
+          </template>
+          <template v-else-if="selectedFeature === 'FUNCTION'">
+            FUNCTION
+          </template>
+          <template v-else-if="selectedFeature === 'INK'">
+            INK
+          </template>
+          <template v-else-if="selectedFeature === 'PACKAGE'">
+            PACKAGE
+          </template>
+      </div>
+    </div>
+      <div class="raka-customize-footer">
+        <button
+          type="button"
+          class="raka-customize-footer__temp-save-btn raka-btn"
+          @click="saveSettings"
+        >
+          暫存設計
+        </button>
+        <button
+          type="button"
+          class="raka-customize-footer__confirm-btn raka-btn"
+        >
+          確認成品
+        </button>
+      </div>
+  </div>
   </div>
 </template>
 
 <script>
+import CustomizeSetter from "@/components/customize/CustomizeSetter.vue";
 export default {
   name: "TheCustomizePage",
+  components: {
+      CustomizeSetter,
+  },
+  data() {
+      return {
+          myWatchTitle: '',
+          selectedFeature: 'SURFACE',
+          featureList: ['SURFACE', 'FUNCTION', 'INK', 'PACKAGE']
+      }
+  },
+  methods: {
+      saveSettings() {
+        const myWatchSettings = {
+          title: this.myWatchTitle,
+          ...this.$refs.customizeSetter.myCustomWatch
+        };
+          console.log('myWatch', myWatchSettings);
+
+      }
+  }
 };
 </script>
 
 <style scoped>
-
+.raka-customize__manu-item.active {
+    color: red;
+}
 </style>
