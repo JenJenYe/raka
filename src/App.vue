7b73566bd2f2b34ba4b504b8d8e7f94967185cc8
JhenJhenYe <dindincn111@gmail.com>
2021-12-05 20:41:48 +0800

    Add Header Component, Footer Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <TheHeader />
+    <router-view />
+  <TheFooter />
 </template>
+
+<script>
+import TheHeader from '@/components/layouts/TheHeader.vue';
+import TheFooter from '@/components/layouts/TheFooter.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    Footer Works
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style scoped>
+
+</style>
